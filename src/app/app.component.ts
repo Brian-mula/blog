501345a57deaf323a0b1d45ad748124869d2f76f
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-13 11:40:14 +0300

    modified app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
-
+import { BlogsComponent } from "./blogs/blogs.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { TopNavComponent } from "./top-nav/top-nav.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, BlogsComponent, SideBarComponent, TopNavComponent,MatIconModule]
 })
 export class AppComponent {
   title = 'blog';
