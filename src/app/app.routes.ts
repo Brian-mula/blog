6d353b8d666a89de7e641815ed55e7b5b5510971
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-14 15:43:47 +0300

    modified app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './app-layout/app-layout.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayoutComponent,
+        children:[
+            {
+                path:'',
+                component:HomeComponent
+            },
+            {
+                path:'blogs',
+                component:BlogsComponent
+            },
+        ]
+    },
+   
+    {
+        path:'**',
+        component: NotFoundComponent
+    }
+];
