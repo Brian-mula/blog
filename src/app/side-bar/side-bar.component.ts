fcabfc7d7db8095f8e31dbff66b49adbcb0c2f1b
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-14 15:45:32 +0300

    added app layout

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,14 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent {
+   links = [
+    {
+      name: 'Home',
+      url: '/'
+    },
+    {
+      name: 'Blogs',
+      url: '/blogs'
+    },
+    {
+      name: 'About',
+      url: '/about'
+    },
+    {
+      name: 'Contact',
+      url: '/contact'
+    },
+   
+   ];
 
 }
