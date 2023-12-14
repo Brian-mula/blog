b20e1e7a48377d4f16333304b29e41d210117e3d
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-14 15:44:32 +0300

    added app layout

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AllBlogsService } from '../services/all-blogs.service';
 import { BlogItemComponent } from "./blog-item/blog-item.component";
+
 
 @Component({
     selector: 'app-blogs',
     standalone: true,
     templateUrl: './blogs.component.html',
     styleUrl: './blogs.component.css',
-    imports: [BlogItemComponent]
+    imports: [BlogItemComponent,CommonModule]
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit{
+    blogs:{title:string,description:string}[] = [];
+
+    constructor(private allBlogs:AllBlogsService) { }
+
+    ngOnInit(): void {
+        this.blogs = this.allBlogs.blogs;
+    }
 
 }
