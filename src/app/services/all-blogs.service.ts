0b0a279ad39daa4c691b4238ee1c5ec864cf7366
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-14 15:45:24 +0300

    added app layout

---
+++ src/app/services/all-blogs.service.ts
@@ -0,0 +1,100 @@
+export class AllBlogsService {
+    constructor() { }
+    blogs =[
+        {
+            title: 'Blog 1',
+            description: 'This is blog 1',
+            id: 1
+        },
+        {
+            title: 'Blog 2',
+            description: 'This is blog 2',
+            id: 2
+        },
+        {
+            title: 'Blog 3',
+            description: 'This is blog 3',
+            id: 3
+        },
+        {
+            title: 'Blog 4',
+            description: 'This is blog 4',
+            id: 4
+        },
+        {
+            title: 'Blog 5',
+            description: 'This is blog 5',
+            id: 5
+        },
+        {
+            title: 'Blog 6',
+            description: 'This is blog 6',
+            id: 6
+        },
+        {
+            title: 'Blog 7',
+            description: 'This is blog 7',
+            id: 7
+        },
+        {
+            title: 'Blog 8',
+            description: 'This is blog 8',
+            id: 8
+        },
+        {
+            title: 'Blog 9',
+            description: 'This is blog 9',
+            id: 9
+        },
+        {
+            title: 'Blog 10',
+            description: 'This is blog 10',
+            id: 10
+        },
+        {
+            title: 'Blog 11',
+            description: 'This is blog 11',
+            id: 11
+        },
+        {
+            title: 'Blog 12',
+            description: 'This is blog 12',
+            id: 12
+        },
+        {
+            title: 'Blog 13',
+            description: 'This is blog 13',
+            id: 13
+        },
+        {
+            title: 'Blog 14',
+            description: 'This is blog 14',
+            id: 14
+        },
+        {
+            title: 'Blog 15',
+            description: 'This is blog 15',
+            id: 15
+        },
+        {
+            title: 'Blog 16',
+            description: 'This is blog 16',
+            id: 16
+        },
+        {
+            title: 'Blog 17',
+            description: 'This is blog 17',
+            id: 17
+        },
+        {
+            title: 'Blog 18',
+            description: 'This is blog 18',
+            id: 18
+        },
+        {
+            title: 'Blog 19',
+            description: 'This is blog 19',
+            id:19
+        }
+    ]
+}
