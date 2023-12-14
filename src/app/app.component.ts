b8877bdd9f42dd343aee3f6c195f8a85ebfd27ce
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-14 15:43:36 +0300

    modified app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { BlogsComponent } from "./blogs/blogs.component";
+import { AllBlogsService } from './services/all-blogs.service';
 import { SideBarComponent } from "./side-bar/side-bar.component";
 import { TopNavComponent } from "./top-nav/top-nav.component";
 @Component({
@@ -10,8 +11,10 @@ import { TopNavComponent } from "./top-nav/top-nav.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, BlogsComponent, SideBarComponent, TopNavComponent,MatIconModule]
+    imports: [CommonModule, RouterOutlet, BlogsComponent, SideBarComponent, TopNavComponent,MatIconModule],
+    providers: [AllBlogsService]
 })
 export class AppComponent {
+  constructor(private allblogsService:AllBlogsService) { }
   title = 'blog';
 }
