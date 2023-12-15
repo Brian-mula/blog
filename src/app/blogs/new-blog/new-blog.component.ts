1008161ea3a71b5056c327e250558452e71a7721
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-15 22:04:05 +0300

    app components

--- src/app/blogs/new-blog/new-blog.component.ts
+++ src/app/blogs/new-blog/new-blog.component.ts
@@ -1,4 +1,6 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Blog } from '../blog.model';
@@ -6,9 +8,9 @@ import { Blog } from '../blog.model';
 @Component({
   selector: 'app-new-blog',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './new-blog.component.html',
-  styleUrl: './new-blog.component.css'
+  styleUrl: './new-blog.component.css',
 })
 export class NewBlogComponent {
   @ViewChild('newBlogModal') newModal: ElementRef;
@@ -16,7 +18,7 @@ export class NewBlogComponent {
 
   //newBlog:Blog = {} as Blog;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   openModal() {
     this.newModal.nativeElement.showModal();
@@ -25,12 +27,19 @@ export class NewBlogComponent {
   closeModal() {
     this.newModal.nativeElement.close();
   }
-  onSubmit(){
-    const newBlog:Blog = {
+  onSubmit() {
+    const newBlog: Blog = {
       title: this.bData.value.title,
-      description: this.bData.value.description
-    }
+      description: this.bData.value.description,
+    };
+    this.http
+      .post(
+        'https://blog-7f459-default-rtdb.firebaseio.com/blogs.json',
+        newBlog
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
     console.log(newBlog);
   }
-
 }
