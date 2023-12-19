55976f07114574e675d3e9f82487ced691719bff
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-19 12:36:30 +0300

    refactored auth logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { LoadingSpinnerComponent } from "../shared/loading-spinner/loading-spinner.component";
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  standalone: true,
-  imports: [FormsModule,CommonModule],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+    selector: 'app-auth',
+    standalone: true,
+    templateUrl: './auth.component.html',
+    styleUrl: './auth.component.css',
+    imports: [FormsModule, CommonModule, LoadingSpinnerComponent]
 })
 export class AuthComponent {
   constructor(
@@ -16,6 +18,9 @@ export class AuthComponent {
   ) {}
 
   isLoginMode:boolean = true;
+  isLoading:boolean = false;
+  error:string = null;
+
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -28,19 +33,24 @@ export class AuthComponent {
     if (!form.valid) {
       return;
     }
+    let authObs:Observable<AuthResponseData>
+    this.isLoading = true;
     if(this.isLoginMode){
-  
+      authObs= this.authService.login(form.value)
     }else{
-      this.authService.signUp(form.value).subscribe({
-        next: responseData => {
-          console.log(responseData);
-        },
-        error: error => {
-          console.log(error);
-        }
-      })
+      authObs= this.authService.signUp(form.value)
     }
-    
+    authObs.subscribe({
+      next: responseData => {
+        this.isLoading = false;
+        console.log(responseData);
+      },
+      error: errorRes => {
+        this.isLoading = false;
+        this.error = errorRes;
+        console.log(errorRes);
+      }
+    })
     form.reset();
   }
 
