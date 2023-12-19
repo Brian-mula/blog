83af933659505603e57005749926d181ffec813c
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-19 09:38:42 +0300

    added http client injecto

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -2,11 +2,9 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { map } from 'rxjs/operators';
-import { AllBlogsService } from '../services/all-blogs.service';
 import { BlogItemComponent } from "./blog-item/blog-item.component";
-import { Blog } from './blog.model';
 import { NewBlogComponent } from "./new-blog/new-blog.component";
+import { PostsService } from './posts.service';
 
 
 @Component({
@@ -20,29 +18,17 @@ export class BlogsComponent implements OnInit{
     blogs:{title:string,description:string}[] = [];
     isFetching = false;
 
-    constructor(private allBlogs:AllBlogsService,private http:HttpClient) { }
+    constructor(private allBlogs:PostsService,private http:HttpClient) { }
 
     ngOnInit(): void {
         
-        this.fetchBlogs();
-    }
-
-    private fetchBlogs(){
         this.isFetching = true;
-        this.http.get('https://blog-7f459-default-rtdb.firebaseio.com/blogs.json').pipe(map((responseData:{[key:string]:Blog}) =>{
-            const blogsArray:Blog[] = [] as Blog[];
-            for(const key in responseData){
-                if(responseData.hasOwnProperty(key)){
-                    blogsArray.push({...responseData[key] as Blog,id:key});
-                }
-            }
-            return blogsArray;
-        }))
-        .subscribe((responseData) => {
-           // console.log(responseData);
-              this.isFetching = false;
-              this.blogs = responseData;
+        this.allBlogs.fetchBlogs().subscribe(blogs => {
+            this.isFetching = false;
+            this.blogs = blogs;
         });
     }
+
+   
 
 }
