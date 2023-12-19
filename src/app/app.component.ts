c5caa8e67bdd059f571b9dbd3e95829a0090216e
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-19 09:38:37 +0300

    added http client injector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
-import { BlogsComponent } from "./blogs/blogs.component";
+import { BlogsComponent } from './blogs/blogs.component';
 import { AllBlogsService } from './services/all-blogs.service';
-import { SideBarComponent } from "./side-bar/side-bar.component";
-import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, BlogsComponent, SideBarComponent, TopNavComponent,MatIconModule,HttpClientModule],
-    providers: [AllBlogsService]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BlogsComponent,
+    SideBarComponent,
+    TopNavComponent,
+    MatIconModule,
+    
+  ],
+  providers: [AllBlogsService,HttpClientModule],
 })
 export class AppComponent {
-  constructor(private allblogsService:AllBlogsService) { }
+  constructor(private allblogsService: AllBlogsService) {}
   title = 'blog';
 }
