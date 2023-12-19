4e07c09d31df7e40977a33028fdc3f55baa1f19a
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-19 11:15:36 +0300

    completed sign up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  constructor(
+     private authService: AuthService
+  ) {}
+
+  isLoginMode:boolean = true;
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+ 
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    if(this.isLoginMode){
+  
+    }else{
+      this.authService.signUp(form.value).subscribe({
+        next: responseData => {
+          console.log(responseData);
+        },
+        error: error => {
+          console.log(error);
+        }
+      })
+    }
+    
+    form.reset();
+  }
 
 }
