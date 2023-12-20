ad83787e96e22f0f0314d0c2db03327771cc5ba8
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-20 11:39:53 +0300

    completed auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { BlogsComponent } from './blogs/blogs.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -8,6 +9,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 export const routes: Routes = [
     {
         path: '',
+        canActivate: [AuthGuard],
         component: AppLayoutComponent,
         children:[
             {
