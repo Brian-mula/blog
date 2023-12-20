f9a620f2d5aa4bce6d05fe872f40667f5da18d94
Brian-mula <mulatiwangilabrian@gmail.com>
2023-12-20 10:59:38 +0300

    added interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { BlogsComponent } from './blogs/blogs.component';
 import { AllBlogsService } from './services/all-blogs.service';
 import { SideBarComponent } from './side-bar/side-bar.component';
@@ -21,7 +22,7 @@ import { TopNavComponent } from './top-nav/top-nav.component';
     MatIconModule,
     
   ],
-  providers: [AllBlogsService,HttpClientModule],
+  providers: [AllBlogsService,{provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}],
 })
 export class AppComponent {
   constructor(private allblogsService: AllBlogsService) {}
